1608e16e1ee90e67b5250e70d76b978ebcea143a
paige <paigevenuto@gmail.com>
2022-06-09 08:49:18 -0400

    better colors

--- config.def.h
+++ config.def.h
@@ -18,17 +18,15 @@ static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Monospace:size=18" };
 static const int statusfontindex    =0;          //select font of font list to use for statusbar
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_chartreuse[]  = "#00ff00";
+static const char col_bg[]          = "#061009";
+static const char col_fg[]          = "#cbe7c7";
 static const char col_green1[]      = "#249368";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_green1 },
-	[SchemeSel]  = { col_gray4, col_green1,  col_chartreuse  },
+	[SchemeNorm] = { col_fg, col_bg, col_chartreuse },
+	[SchemeSel]  = { col_fg, col_green1,  col_chartreuse  },
 };
 
 /* tagging */
